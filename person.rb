fd5418a7a19a952b65d7f91743de52fd728da79e
Joseph Buckwell <jbuckwell@spartaglobal.com>
2015-11-18 14:09:10 +0000

    finished test case store and return phone numbers

--- person.rb
+++ person.rb
@@ -2,12 +2,13 @@ require 'date'
 
 class Person
   attr_accessor :dob,:first_name,:surname
-  attr_reader :emails
+  attr_reader :emails, :phone_numbers
   def initialize(first_name,surname,dob)
     @dob = Date.parse(dob)
     @first_name = first_name.capitalize
     @surname = surname.capitalize
     @emails = Array.new
+    @phone_numbers = Array.new
   end
 
   def fullname
@@ -18,4 +19,11 @@ class Person
   def add_email(email)
     emails.push(email)
   end
+
+  #add phone
+  def add_phone(number)
+    phone_numbers.push(number)
+  end
+
+  
 end
